0ff9724f985b429913ea645739dd025b03d878b5
Mary-Wynn Rogers <DrkAngl90@gmail.com>
2014-01-10 19:50:27 -0500

    calculation of skill and stats

--- LootTheRoom/src/com/toastercat/loottheroom/game/Character.java
+++ LootTheRoom/src/com/toastercat/loottheroom/game/Character.java
@@ -6,14 +6,29 @@ public class Character
     private int iAttribute;
 
     // Attributes
+    //modified
     private float fHealthModifier;
     private float fDevraModifier;
     private float fStaminaModifier;
+    //base
     private float fHealth;
     private float fDevra;
     private float fStamina;
 
-    // Skills
+    // Skill
+    private float fEnergyWeapons;
+    private float fExplosives;
+    private float fGuns;
+    private float fMeleeWeapons;
+    private float fUnarmed;
+    private float fHeavyArmor;
+    private float fBarter;
+    private float fMedicine;
+    private float fRepair;
+    private float fSpeech;
+    private float fLightArmor;
+
+    // Skill Modifier
     private float fEnergyWeaponsModifier;
     private float fExplosivesModifier;
     private float fGunsModifier;
@@ -27,21 +42,35 @@ public class Character
     private float fLightArmorModifier;
 
     // Skill Rates
-    private double fEnergyWeaponsSkillRate;
-    private double fExplosivesSkillRate;
-    private double fGunsSkillRate;
-    private double fMeleeWeaponsSkillRate;
-    private double fUnarmedSkillRate;
-    private double fHeavyArmorSkillRate;
-    private double fBarterSkillRate;
-    private double fMedicineSkillRate;
-    private double fRepairSkillRate;
-    private double fSpeechSkillRate;
-    private double fLightArmorSkillRate;
+    private double dEnergyWeaponsSkillRate;
+    private double dExplosivesSkillRate;
+    private double dGunsSkillRate;
+    private double dMeleeWeaponsSkillRate;
+    private double dUnarmedSkillRate;
+    private double dHeavyArmorSkillRate;
+    private double dBarterSkillRate;
+    private double dMedicineSkillRate;
+    private double dRepairSkillRate;
+    private double dSpeechSkillRate;
+    private double dLightArmorSkillRate;
 
     public Character()
     {
+        fHealth = 100;
+        fDevra = 100;
+        fStamina = 100;
 
+        fEnergyWeapons = 15;
+        fExplosives = 15;
+        fGuns = 15;
+        fMeleeWeapons = 15;
+        fUnarmed = 15;
+        fHeavyArmor = 15;
+        fBarter = 15;
+        fMedicine = 15;
+        fRepair = 15;
+        fSpeech = 15;
+        fLightArmor = 15;
     }
 
     // ----------------------------------------------------------
@@ -54,19 +83,44 @@ public class Character
         ApplyAttributes();
     }
 
-
-    private void ApplyAttributes()
+    /**
+     * applies an increase to attributes
+     */
+    public void ApplyAttributes()
     {
         // TODO Auto-generated method stub
         // get the base attribute and add modified
+        fHealth = fHealth + fHealthModifier;
+        fDevra = fDevra + fDevraModifier;
+        fStamina = fStamina + fStaminaModifier;
+    }
 
+    /**
+     * unapply an increase to attributes
+     */
+    public void UnapplyAttributes()
+    {
+     // get the base attribute and minus modified
+        fHealth = fHealth - fHealthModifier;
+        fDevra = fDevra - fDevraModifier;
+        fStamina = fStamina - fStaminaModifier;
     }
 
     private void ApplySkillValues()
     {
         // TODO Auto-generated method stub
         // get base skill and add modified
-
+        fEnergyWeapons = fEnergyWeapons + fEnergyWeaponsModifier;
+        fExplosives = fExplosives + fExplosivesModifier;
+        fGuns = fGuns + fGunsModifier;
+        fMeleeWeapons = fMeleeWeapons + fMeleeWeaponsModifier;
+        fUnarmed = fUnarmed + fUnarmedModifier;
+        fHeavyArmor = fHeavyArmor + fHeavyArmorModifier;
+        fBarter = fBarter + fBarterModifier;
+        fMedicine = fMedicine + fMedicineModifier;
+        fRepair = fRepair + fRepairModifier;
+        fSpeech = fSpeech + fSpeechModifier;
+        fLightArmor = fLightArmor + fLightArmorModifier;
     }
 
 
@@ -99,22 +153,26 @@ public class Character
         fLightArmorModifier = 0;
     }
 
+    public void setSkillValueModifiers()
+    {
+        int aiElement = 0;
+    }
     /**
      * resets the skill rates to 100%
      */
     public void resetSkillRates()
     {
-        fEnergyWeaponsSkillRate = 1.00;
-        fExplosivesSkillRate = 1.00;
-        fGunsSkillRate = 1.00;
-        fMeleeWeaponsSkillRate = 1.00;
-        fUnarmedSkillRate = 1.00;
-        fHeavyArmorSkillRate = 1.00;
-        fBarterSkillRate = 1.00;
-        fMedicineSkillRate = 1.00;
-        fRepairSkillRate = 1.00;
-        fSpeechSkillRate = 1.00;
-        fLightArmorSkillRate = 1.00;
+        dEnergyWeaponsSkillRate = 1.00;
+        dExplosivesSkillRate = 1.00;
+        dGunsSkillRate = 1.00;
+        dMeleeWeaponsSkillRate = 1.00;
+        dUnarmedSkillRate = 1.00;
+        dHeavyArmorSkillRate = 1.00;
+        dBarterSkillRate = 1.00;
+        dMedicineSkillRate = 1.00;
+        dRepairSkillRate = 1.00;
+        dSpeechSkillRate = 1.00;
+        dLightArmorSkillRate = 1.00;
     }
 
 }
